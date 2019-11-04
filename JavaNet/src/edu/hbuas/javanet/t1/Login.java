eca946395ba3d112c84168862a95a715731c836c
Lenovo <Lenovo@dsj-45>
2019-11-04 10:42:30 +0800

    first commit

---
+++ JavaNet/src/edu/hbuas/javanet/t1/Login.java
@@ -0,0 +1,29 @@
+package edu.hbuas.javanet.t1;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * scene Builder
+ * @author Lenovo
+ */
+public class Login  extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Group g=new Group();
+		Scene  s=new Scene(g,300,400,Color.GRAY);
+		primaryStage.setTitle("Login");
+		primaryStage.setScene(s);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		Login.launch(args);
+	}
+
+}
